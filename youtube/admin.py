4425605e2e936b155965baf769a30814f25590b2
Shoban <shoban05@bitbucket.org>
2020-09-18 00:04:39 +0530

    Change in Django Admin for VideoData Dashboard

--- youtube/admin.py
+++ youtube/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import VideoData
 # Register your models here.
+
+
+@admin.register(VideoData)
+class VideoDataAdmin(admin.ModelAdmin):
+    fields = ('title', 'description', 'publishing_datetime')
+    list_display = ('title',)
+    list_filter = ('title', 'publishing_datetime')
+    ordering = ('-publishing_datetime',)
+    search_fields = ('title__icontains', 'description__icontains')
+    date_hierarchy = 'publishing_datetime'
+    pass
